cbd7d5c04a447f7241bcbc8c41d4822ca29959b6
wilson385 <wilson.cristvao@gmail.com>
2024-09-06 12:57:34 -0300

    Aula de música + Cerificado
    Estou desenvolvendo um site que oferecerá acesso gratuito a aulas de música para todos. Além das aulas, também será possível obter certificados de conclusão, sem nenhum custo. O objetivo é proporcionar uma oportunidade acessível para que qualquer pessoa possa aprender música e obter reconhecimento pelo seu progresso.

---
+++ dados.js
@@ -0,0 +1,67 @@
+let dados = [ 
+    {
+        titulo: "Bateria",
+        descricao: `Durante um curso de bateria, você aprenderá:
+        Técnica Básica: Posicionamento, técnica de baquetas e coordenação entre mãos e pés.
+        Leitura de Partituras: Interpretação e execução de ritmos e grooves.
+        Coordenação Motora: Desenvolvimento da habilidade de usar independência das mãos e pés.
+        Estilos Musicais: Adaptação a diferentes estilos, como rock, jazz e funk.
+        Percepção Auditiva: Identificação e ajuste de tempos, acentos e dinâmicas.
+        Improvisação e Criatividade: Criação de padrões rítmicos originais e exploração musical.
+        Trabalho em Equipe: Colaboração com outros músicos e prática em grupo.
+        Disciplina e Autoavaliação: Melhoria contínua através da prática e análise crítica.`,
+        link: "https://www.cifraclub.com.br/blog/partes-da-bateria/#google_vignette/"
+    },
+    {
+        titulo: "Teclado",
+        descricao: `Durante um curso de teclado, você aprenderá:
+        Técnica Básica: Posicionamento das mãos, escalas e acordes.
+        Leitura de Partituras: Interpretação de partituras e cifras.
+        Coordenação Motora: Desenvolvimento da habilidade de tocar com ambas as mãos.
+        Estilos Musicais: Adaptação a diferentes estilos, como pop, clássico e jazz.
+        Percepção Auditiva: Identificação de acordes e melodias.
+        Improvisação e Criatividade: Criação de arranjos e solos originais.
+        Trabalho em Equipe: Colaboração com outros músicos e prática em grupo.
+        Disciplina e Autoavaliação: Melhoria contínua através da prática e análise crítica.`,
+        link: "https://www.cifraclub.com.br/blog/partes-do-teclado/"
+    },
+    {
+        titulo: "Saxofone",
+        descricao: `Durante um curso de saxofone, você aprenderá:
+        Técnica Básica: Posicionamento, embocadura e respiração.
+        Leitura de Partituras: Interpretação de partituras e improvisação.
+        Coordenação Motora: Desenvolvimento da habilidade de tocar com fluidez.
+        Estilos Musicais: Adaptação a diferentes estilos, como jazz, blues e rock.
+        Percepção Auditiva: Identificação de notas e frases musicais.
+        Improvisação e Criatividade: Criação de solos e melodias originais.
+        Trabalho em Equipe: Colaboração com outros músicos e prática em grupo.
+        Disciplina e Autoavaliação: Melhoria contínua através da prática e análise crítica.`,
+        link: "https://www.cifraclub.com.br/blog/partes-do-saxofone/"
+    },
+    {
+        titulo: "Guitarra",
+        descricao: `Durante um curso de guitarra, você aprenderá:
+        Técnica Básica: Posicionamento das mãos, acordes e escalas.
+        Leitura de Partituras: Interpretação de tablaturas e partituras.
+        Coordenação Motora: Desenvolvimento da habilidade de tocar solos e riffs.
+        Estilos Musicais: Adaptação a diferentes estilos, como rock, blues e pop.
+        Percepção Auditiva: Identificação de acordes e melodias.
+        Improvisação e Criatividade: Criação de solos e composições originais.
+        Trabalho em Equipe: Colaboração com outros músicos e prática em grupo.
+        Disciplina e Autoavaliação: Melhoria contínua através da prática e análise crítica.`,
+        link: "https://www.cifraclub.com.br/blog/partes-da-guitarra/"
+    },
+    {
+        titulo: "Baixo",
+        descricao: `Durante um curso de baixo, você aprenderá:
+        Técnica Básica: Posicionamento, técnica de dedilhado e slap.
+        Leitura de Partituras: Interpretação de tablaturas e cifras.
+        Coordenação Motora: Desenvolvimento da habilidade de tocar linhas de baixo.
+        Estilos Musicais: Adaptação a diferentes estilos, como funk, rock e jazz.
+        Percepção Auditiva: Identificação de grooves e ritmos.
+        Improvisação e Criatividade: Criação de linhas de baixo originais.
+        Trabalho em Equipe: Colaboração com outros músicos e prática em grupo.
+        Disciplina e Autoavaliação: Melhoria contínua através da prática e análise crítica.`,
+        link: "https://www.cifraclub.com.br/blog/partes-do-baixo/"
+    }
+];
